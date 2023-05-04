e50bbffee2ea47761087951de80005b03ea18c66
David Pittman <dp205571@pennwest.edu>
2023-05-04 20:40:26 +0000

    Replace userT.h

--- UserT class + UserListT class + userTest.cpp/userT.h
+++ UserT class + UserListT class + userTest.cpp/userT.h
@@ -5,6 +5,13 @@
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <cmath>
+
+/*
+To Do:
+Function to create a user repo and add an event to it. (Date,Type,Distance,Time)
+
+*/
 
 const std::string USER_FILE {"users.txt"};
 
@@ -18,9 +25,19 @@ UserT(std::string username, int ID);
 std::string GetUser();
 int GetID();
 void Print();
+void SetSwim(float num);
+void SetRun(float num);
+void SetBike(float num);
+void SwimCheck(float speed);
+void RunCheck(float speed);
+void BikeCheck(float speed);
+void CheckTargets();
+void OutputGraph(float num);
 
 private:
 std::string name;
 int UserID;
-
+float targetSwim;
+float targetRun;
+float targetBike;
 };
